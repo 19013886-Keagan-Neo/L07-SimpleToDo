b2a89786e693cc0978c4d9038bf9967dd7de440a
19013886 <19013886@myrp.edu.sg>
2021-06-08 16:26:12 +0800

    P07 - PS Enhancement Done

--- app/src/main/java/sg/edu/rp/c346/id19013886/l07_simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19013886/l07_simpletodo/MainActivity.java
@@ -3,19 +3,25 @@ package sg.edu.rp.c346.id19013886.l07_simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etToDo;
-    Button btnAdd, btnClear;
+    Button btnAdd, btnClear, btnDelete;
     ListView lvTask;
+
+    Spinner spnAddRemove;
 
     ArrayList<String> alTask;
     ArrayAdapter<String> aaTask;
@@ -27,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         etToDo = findViewById(R.id.editTextToDo);
         btnAdd = findViewById(R.id.btnAdd);
+        btnDelete = findViewById(R.id.btnDelete);
         btnClear = findViewById(R.id.btnClear);
         lvTask = findViewById(R.id.listViewTask);
+        spnAddRemove = findViewById(R.id.spinnerAddRemove);
 
         alTask = new ArrayList<>();
 
@@ -51,12 +59,67 @@ public class MainActivity extends AppCompatActivity {
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String task = etToDo.getText().toString();
                 alTask.clear();
                 aaTask.notifyDataSetChanged();
             }
         });
 
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etToDo.getText().toString().isEmpty()){
+                    int pos = Integer.parseInt(etToDo.getText().toString());
+                    if (aaTask.getCount() > pos) {
+                        alTask.remove(pos);
+                    } else if (aaTask.getCount() == 0) {
+                        Toast.makeText(MainActivity.this, "You don't have any task to remove",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Wrong index number", Toast.LENGTH_LONG).show();
+                    }
+                    Toast.makeText(MainActivity.this, "Index " + pos + " have been deleted!",
+                            Toast.LENGTH_LONG).show();
+
+                    aaTask.notifyDataSetChanged();
+                    etToDo.setText("");
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Index is empty! Please enter something!",
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        spnAddRemove.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        // Add button
+                        etToDo.setHint(R.string.addHint);
+                        etToDo.setInputType(InputType.TYPE_CLASS_TEXT);
+                        etToDo.setText("");
+                        btnAdd.setEnabled(true);
+                        btnDelete.setEnabled(false);
+                        break;
+
+                    case 1:
+                        // Delete button
+                        etToDo.setHint(R.string.removeHint);
+                        etToDo.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        etToDo.setText("");
+                        btnAdd.setEnabled(false);
+                        btnDelete.setEnabled(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
